c1c7461490a1357b4ee6a4451421fee7b52d555c
ChrisB007 <chrisbitoy@gmail.com>
2021-02-27 23:51:11 -0500

    canabeast update

--- pages/index.js
+++ pages/index.js
@@ -239,14 +239,14 @@ export default function Home() {
       </li>
       <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200">
         <div className="flex-1 flex flex-col p-8">
-          <img className="w-64 h-32 flex-shrink-0 mx-auto bg-black rounded-t-lg" src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt />
-          <h3 className="mt-6 text-gray-900 text-sm font-medium">Theresa Webb</h3>
+          <img className="w-64 h-32 flex-shrink-0 mx-auto bg-black rounded-t-lg" src="images/canabeast.png" alt />
+          <h3 className="mt-6 text-gray-900 text-sm font-medium">Canabeast</h3>
           <dl className="mt-1 flex-grow flex flex-col justify-between">
             <dt className="sr-only">Title</dt>
-            <dd className="text-gray-500 text-sm">Global Division Officer</dd>
+            <dd className="text-gray-500 text-sm">Everything you need to know about the Cannabis industry</dd>
             <dt className="sr-only">Role</dt>
             <dd className="mt-3">
-              <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">Admin</span>
+              <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">Web | Mobile</span>
             </dd>
           </dl>
         </div>
@@ -259,7 +259,7 @@ export default function Home() {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
-                <span className="ml-3">Email</span>
+                <span className="ml-3">Website</span>
               </a>
             </div>
             <div className="-ml-px w-0 flex-1 flex">
@@ -268,7 +268,7 @@ export default function Home() {
                 <svg className="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
-                <span className="ml-3">Call</span>
+                <span className="ml-3">Visit</span>
               </a>
             </div>
           </div>
