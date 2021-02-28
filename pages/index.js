4019ed7abbd6b3d54c5ecb10d152eaddd3cfdefd
ChrisB007 <chrisbitoy@gmail.com>
2021-02-28 01:21:10 -0500

    logo for canabeast update

--- pages/index.js
+++ pages/index.js
@@ -239,7 +239,7 @@ export default function Home() {
       </li>
       <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200">
         <div className="flex-1 flex flex-col p-8">
-          <img className="w-64 h-32 flex-shrink-0 mx-auto bg-black rounded-t-lg" src="images/canabeast.png" alt />
+          <img className="w-64 h-32 flex-shrink-0 mx-auto bg-black rounded-t-lg" src="images/canab.png" alt />
           <h3 className="mt-6 text-gray-900 text-sm font-medium">Cannabeast</h3>
           <dl className="mt-1 flex-grow flex flex-col justify-between">
             <dt className="sr-only">Title</dt>
