24baa1b3f7d2eec279ae489ed56ece08a4e1696f
ChrisB007 <chrisbitoy@gmail.com>
2021-03-01 18:30:17 -0500

    boxoff

--- pages/index.js
+++ pages/index.js
@@ -253,11 +253,11 @@ export default function Home() {
       </li>
       <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200">
         <div className="flex-1 flex flex-col p-8">
-          <img className="w-64 h-32 flex-shrink-0 mx-auto bg-black rounded-t-lg" src="images/canab.png" alt />
-          <h3 className="mt-6 text-gray-900 text-sm font-medium">Cannabeast</h3>
+          <img className="w-64 h-32 flex-shrink-0 mx-auto bg-black rounded-t-lg" src="images/boxoff.png" alt />
+          <h3 className="mt-6 text-gray-900 text-sm font-medium">BoxOff</h3>
           <dl className="mt-1 flex-grow flex flex-col justify-between">
             <dt className="sr-only">Title</dt>
-            <dd className="text-gray-500 text-sm">Resource for everything in the Cannabis industry</dd>
+            <dd className="text-gray-500 text-sm">Make educated guess on which movie will rule the box office</dd>
             <dt className="sr-only">Role</dt>
             <dd className="mt-3">
               <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">Web | Mobile</span>
