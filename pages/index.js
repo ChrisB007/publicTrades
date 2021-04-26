493db8e2502571cb4108617c9fd65f04fddf60bf
ChrisB007 <chrisbitoy@gmail.com>
2021-04-26 12:26:02 -0400

    removed unneeded head element

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
               <div className="-mr-2 flex items-center md:hidden">
                 <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" id="main-menu" aria-haspopup="true">
                   <span className="sr-only">Open main menu</span>
-                  {/* Heroicon name: outline/menu */}
+                  
                   <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                   </svg>
@@ -58,20 +58,6 @@ export default function Home() {
 */}
       <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
         <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-          <div className="px-5 pt-4 flex items-center justify-between">
-            <div>
-              <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt />
-            </div>
-            <div className="-mr-2">
-              <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                <span className="sr-only">Close main menu</span>
-                {/* Heroicon name: outline/x */}
-                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-          </div>
           {/* <div role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
             <div className="px-2 pt-2 pb-3 space-y-1" role="none">
               <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Services</a>
@@ -336,17 +322,7 @@ export default function Home() {
             <ul className="mt-4 space-y-4">
               <li>
                 <a href="#" className="text-base text-gray-300 hover:text-white">
-                  Documentation
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-base text-gray-300 hover:text-white">
-                  Guides
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-base text-gray-300 hover:text-white">
-                  API Status
+                  API
                 </a>
               </li>
             </ul>
